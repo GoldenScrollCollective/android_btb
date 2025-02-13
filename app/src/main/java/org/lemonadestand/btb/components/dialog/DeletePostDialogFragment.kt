9eec7f6852151898c1b2e85295201ce565b12995
Alone <bd901029@hotmail.com>
2025-02-13 12:20:27 -0300

    added resend limit timer

--- app/src/main/java/org/lemonadestand/btb/components/dialog/DeletePostDialogFragment.kt
+++ app/src/main/java/org/lemonadestand/btb/components/dialog/DeletePostDialogFragment.kt
@@ -6,6 +6,9 @@ import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.lemonadestand.btb.App
 import org.lemonadestand.btb.R
 import org.lemonadestand.btb.components.base.BaseDialogFragment
@@ -13,6 +16,8 @@ import org.lemonadestand.btb.components.base.BaseFragment
 import org.lemonadestand.btb.core.manager.PostsManager
 import org.lemonadestand.btb.core.manager.UserManager
 import org.lemonadestand.btb.extensions.setOnSingleClickListener
+import java.util.Timer
+import kotlin.concurrent.scheduleAtFixedRate
 
 class DeletePostDialogFragment @JvmOverloads constructor(
 	fragment: BaseFragment,
@@ -24,6 +29,9 @@ class DeletePostDialogFragment @JvmOverloads constructor(
 	private lateinit var loadingView: LinearLayout
 	private lateinit var loadedView: LinearLayout
 	private lateinit var statusView: TextView
+	private lateinit var btnResend: TextView
+
+	private var resendLimit = 60
 
 	override fun init() {
 		super.init()
@@ -39,7 +47,7 @@ class DeletePostDialogFragment @JvmOverloads constructor(
 		loadedView = rootView.findViewById(R.id.loadedView)
 		statusView = rootView.findViewById(R.id.statusView)
 
-		val btnResend = rootView.findViewById<TextView>(R.id.btnResend)
+		btnResend = rootView.findViewById(R.id.btnResend)
 		btnResend.setOnSingleClickListener { handleSendCode() }
 
 		val btnCancel = rootView.findViewById<TextView>(R.id.btnCancel)
@@ -55,6 +63,28 @@ class DeletePostDialogFragment @JvmOverloads constructor(
 		handleSendCode()
 	}
 
+	private fun startResendLimitTimer() {
+		resendLimit = 60
+		val timer = Timer()
+		timer.scheduleAtFixedRate(0, 1000) {
+			resendLimit--
+			if (resendLimit <= 0) {
+				CoroutineScope(Dispatchers.Main).launch {
+					btnResend.text = "Resend"
+					btnResend.isClickable = true
+				}
+
+				timer.cancel()
+				return@scheduleAtFixedRate
+			}
+
+			CoroutineScope(Dispatchers.Main).launch {
+				btnResend.text = "Resend in ${resendLimit}s"
+				btnResend.isClickable = false
+			}
+		}
+	}
+
 	private fun handleSendCode() {
 		loadingView.visibility = View.VISIBLE
 		loadedView.visibility = View.GONE
@@ -67,6 +97,8 @@ class DeletePostDialogFragment @JvmOverloads constructor(
 				statusView.text = "Something went wrong and please try again."
 			} else {
 				statusView.text = "We sent code to your email."
+
+				startResendLimitTimer()
 			}
 		}
 	}
@@ -80,6 +112,7 @@ class DeletePostDialogFragment @JvmOverloads constructor(
 
 		PostsManager.deletePost(uniqueId, code) {
 			onDeleted.invoke()
+			dismiss()
 		}
 	}
 }
